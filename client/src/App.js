f2a9daab800b397b2acc41049e3d01459a2e1e23
cyreilv7 <shoiiru3@gmail.com>
2022-03-26 00:56:58 -0700

    Move index.js to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
+import './App.scss';
 import Home from './Home'
 import Charities from './Charities';
 // import { BrowserRouter, Routes, Route} from 'react-router-dom';
