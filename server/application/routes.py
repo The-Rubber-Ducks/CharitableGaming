2e83b5414848cc107b97bb7cfb316b1e0426d734
jscervantes <jscervantes00@gmail.com>
2022-03-26 16:08:45 -0400

    Add routes.py, login and register routes

---
+++ server/application/routes.py
@@ -0,0 +1,30 @@
+from flask import render_template, url_for, redirect, jsonify
+
+
+@app.route("/api/login", methods=['POST'])
+def login():
+    if request.method == "POST":
+        # Need to make these forms
+        email=request.form.get("email")
+        password=request.form.get("password")
+        # Authenticate
+
+
+
+    return abort(405)
+
+@app.route("/api/register", methods=['POST'])
+def register():
+    if request.method == "POST":
+        # Return array of games and player handles
+        email=request.form.get("email") # email input field goes here
+        password=request.form.get("password") # password input field goes here
+        confirmpassword=request.form.get("confirmpassword") 
+        if password != confirmpassword:
+            return abort(400)
+        # Add new user to firebase
+        # Authenticate
+        # Add player names to database
+
+
+    return abort(405)
