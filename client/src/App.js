ab9b0e4c5a321258d999e69348bf8097ec40ba39
Jawad Khawaja <devjawkha@gmail.com>
2022-03-27 19:42:03 +0500

    specify routes for new screens

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import Home from './Home'
 import AboutUs from './AboutUs';
 import Dashboard from './Dashboard'
 import Charities from './Charities';
+import Login from './Login';
 import Register from './Register';
+import ResetPassword from './ResetPassword'
 import NotFound from './NotFound';
 import NavigationBar from './NavigationBar';
 import Footer from './Footer';
@@ -16,7 +18,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<AboutUs />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/charities" element={<Charities />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<NotFound />} />
