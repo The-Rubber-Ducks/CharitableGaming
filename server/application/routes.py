c92a948bc926d1ec49a51af441c05ce27a77919e
jscervantes <jscervantes00@gmail.com>
2022-03-26 22:55:16 -0400

    Add .swp files to .gitignore, add routes

--- server/application/routes.py
+++ server/application/routes.py
@@ -3,6 +3,8 @@ from application import app, fbase
 from .FirebaseFuncs.FirebaseFuncs import CurrentUserNotSet, UserAuthenticationError, UserTokenError
 from firebase_admin import auth, exceptions
 import json
+import requests
+import RiotWatcher
 
 
 @app.route("/api/login", methods=['POST'])
@@ -70,10 +72,63 @@ def register():
         else:
             return  json.dumps({'success': True}), 200, {'ContentType':'application/json'}
 
-
     return abort(405)
 
 
+@app.route("/api/get_all_charities")
+def get_all_charities():
+    if request.method == "GET":
+        # Call database functions to get charities
+        return  json.dumps({'success': True}), 200, {'ContentType':'application/json'}
+    return abort(405)
+
+
+@app.route("/api/set_charity", methods=["POST"])
+def set_charity():
+    if request.method == "POST":
+        set_charity_response = request.get_json()
+        try: 
+            charity_name = set_charity_response['charity_name']
+            if not charity_name:
+                return abort(400)
+            fbase.set_user_charity(charity_name)
+
+        except KeyError:
+            return abort(400)
+        except exceptions.NotFoundError:
+            return abort(400)
+        else:
+            return  json.dumps({'success': True}), 200, {'ContentType':'application/json'}
+
+    return abort(405) 
+
+
+@app.route("/api/logout")
+def logout():
+    if request.method == "GET":
+        try:
+            fbase.logout_user()
+            return  json.dumps({'success': True}), 200, {'ContentType':'application/json'}
+        except firebase_admin.exceptions.FirebaseError:
+            return abort(400)
+
+    return abort(405)
+        
+
+@app.route("/api/get_user_league_games")
+def get_user_league_games():
+    # Get the player's GameID
+    # Call Will's Riot API to get the games
+    # Add a check for game in database
+    # Add games to database
+    # Send games to frontend
+    if request.method == "GET":
+        try:
+            summoner_name = fbase.get_user_player_id()
+            
+
+
+    return abort(405)
 
 #@app.route("/api/get_user_league_games")
 #def get_league_games():
