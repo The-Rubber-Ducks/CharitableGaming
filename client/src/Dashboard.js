6a04c78445290772a2a2e41b4bc1eebf4f23565f
cyreilv7 <shoiiru3@gmail.com>
2022-03-26 19:24:09 -0700

    Charity page css refactor

---
+++ client/src/Dashboard.js
@@ -0,0 +1,10 @@
+import MatchHistory from "./MatchHistory";
+const Dashboard = () => {
+    return (  
+        <div className="">
+            <MatchHistory gameTitle={"League of Legends"}/>
+        </div>
+    );
+}
+ 
+export default Dashboard;
---
+++ client/src/MatchHistory.js
@@ -0,0 +1,11 @@
+import MatchList from "./MatchList";
+
+const MatchHistory = ({ gameTitle }) => {
+    return (  
+        <div className="match-history-container">
+            <MatchList />
+        </div>
+    );
+}
+ 
+export default MatchHistory;
