c62a3fc329ba31db41dfc7e8de5abdd0fc9efc0a
Jawad Khawaja <devjawkha@gmail.com>
2022-03-27 16:23:35 +0500

    add tailwind to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import Home from './Home'
 import Dashboard from './Dashboard'
 import Charities from './Charities';
 import Register from './Register';
 import NavigationBar from './NavigationBar';
+import Footer from './Footer';
 
-function App() {  
+function App() {
   return (
-      <div className="App">
-        <BrowserRouter>
-          <NavigationBar />
-          <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/charities" element={<Charities />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/charities" element={<Charities />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
