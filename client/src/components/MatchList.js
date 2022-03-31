4c1d0d2d2cb9b17665772bfb2aa8647c0f00efe7
cyreilv7 <shoiiru3@gmail.com>
2022-03-31 12:30:29 -0700

    Delete unused files

--- client/src/components/MatchList.js
+++ client/src/components/MatchList.js
@@ -4,7 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
 
-const MatchList = ({ matches }) => {
+const MatchList = ({ gameTitle, matches }) => {
     matches.forEach(match => {
         let factor;
         (match.win) ? factor = 2 : factor = 1; 
@@ -14,49 +14,60 @@ const MatchList = ({ matches }) => {
 
     const imageSize = "30px";
     return (
-      <ListGroup className="match-list-container">
-          {matches.map((match, i) => ( 
-            <ListGroup.Item key={i} className={match.win ? "victory" : "defeat"}>
-                <Row>
-                    <Col className="stat-container win-loss">
-                        {match.win ? "VICTORY" : "DEFEAT"} 
-                    </Col>
-                    <Col className="stat-container">
-                        <img 
-                            src="kill.png" 
-                            width={imageSize}
-                            height={imageSize}
-                            alt="Logo" 
-                            className="stat-logo"
-                        />
-                        {match.kills} { (match.kills !== 1) ? "kills" : "kill"}
-                    </Col>
-                    <Col className="stat-container">
-                        <img 
-                                src="death.png" 
-                                width={imageSize}
-                                height={imageSize}
-                                alt="Logo" 
-                                className="stat-logo"
-                        />
-                        {match.deaths} { (match.deaths !== 1) ? "deaths" : "death"}
-                        </Col>
-                    <Col className="stat-container">
-                        <img 
-                            src="assist.png" 
-                            width={imageSize}
-                            height={imageSize}
-                            alt="Logo" 
-                            className="stat-logo"
-                        />
-                        {match.assists} { (match.assists !== 1) ? "assists" : "assist"}
-                    </Col>
-                    <Col className="stat-container">+ {match.charityPoints} CP</Col>
-                </Row>
+        <div>
+            <div className="text-center">
+                <h2 className="game-header mb-0"> { gameTitle } </h2>
+                <p className="mb-1"> (Last 5 matches)</p>
+            </div>
 
-            </ListGroup.Item>
-          ))}
-      </ListGroup>
+            {/* <h2 className="text-center mb-0"> { gameTitle } </h2>
+            <p className="mb-1"> Last 5 matches</p> */}
+
+            <ListGroup className="match-list-container">
+                {matches.map((match, i) => ( 
+                  <ListGroup.Item key={i} className={(match.win ? "victory" : "defeat") + " stats-row"}>
+                      {/* <Row> */}
+                          <Col className="stat-col win-loss">
+                              {match.win ? "VICTORY" : "DEFEAT"} 
+                          </Col>
+                          <Col className="stat-col">
+                              <img 
+                                  src="kill.png" 
+                                  width={imageSize}
+                                  height={imageSize}
+                                  alt="Logo" 
+                                  className="stat-logo"
+                              />
+                              {match.kills} { (match.kills !== 1) ? "kills" : "kill"}
+                          </Col>
+                          <Col className="stat-col">
+                              <img 
+                                      src="death.png" 
+                                      width={imageSize}
+                                      height={imageSize}
+                                      alt="Logo" 
+                                      className="stat-logo"
+                              />
+                              {match.deaths} { (match.deaths !== 1) ? "deaths" : "death"}
+                              </Col>
+                          <Col className="stat-col">
+                              <img 
+                                  src="assist.png" 
+                                  width={imageSize}
+                                  height={imageSize}
+                                  alt="Logo" 
+                                  className="stat-logo"
+                              />
+                              {match.assists} { (match.assists !== 1) ? "assists" : "assist"}
+                          </Col>
+                          <Col className="stat-col">+ {match.charityPoints} CP</Col>
+                      {/* </Row> */}
+      
+                  </ListGroup.Item>
+                ))}
+            </ListGroup>
+        </div>
+
     )
   }
  
